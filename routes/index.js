b98c361d6164666e9b3af9b10d4db8593d4eebbb
peddaswamy <kpswamy540@gmail.com>
2021-01-27 19:08:09 +0530

    added count and delete fucntionality

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,19 @@
+const envs=require('dotenv').config();
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
+const url = 'mongodb://'+process.env.FNXT_MONGO_HOST+':'+process.env.FNXT_MONGO_PORT;
+console.log(url);
+const dbName = process.env.FNXT_MONGO_DBNAME;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/salesorder',(req,res,nxt)=>{
-	const MongoClient = require('mongodb').MongoClient;
-	const assert = require('assert');
-	 
-	// Connection URL
-	const url = 'mongodb://13.234.66.77:27017';
-	 
-	// Database Name
-	const dbName = 'myproject';
-	 
+	
 	// Use connect method to connect to the server
 	MongoClient.connect(url, function(err, client) {
 	  assert.equal(null, err);
@@ -23,15 +21,38 @@ router.post('/salesorder',(req,res,nxt)=>{
 	 
 	  const db = client.db(dbName);
 	 
-	  insertDocuments(db, function() {
+	  insertDocuments(db,req.body,'salesorders', function() {
 	    client.close();
+	    res.json({
+            status:'1',
+            msg:'salesorder data uploaded.',
+           
+        });
 	  });
 	});
 });
-
-const insertDocuments = function(db,data,callback) {
-  // Get the documents collection
-  const collection = db.collection('documents');
+router.post('/salesinvoice',(req,res,nxt)=>{
+	
+	// Use connect method to connect to the server
+	MongoClient.connect(url, function(err, client) {
+	  assert.equal(null, err);
+	  console.log("Connected successfully to server");
+	 
+	  const db = client.db(dbName);
+	 
+	  insertDocuments(db,req.body,'salesinvoices', function() {
+	    client.close();
+	    res.json({
+            status:'1',
+            msg:'salesinovice data uploaded.',
+           
+        });
+	  });
+	});
+});
+const insertDocuments = function(db,data,service,callback) {
+  
+  const collection = db.collection(service);
   // Insert some documents
   collection.insertOne(data, function(err, result) {
     assert.equal(err, null);
@@ -41,22 +62,54 @@ const insertDocuments = function(db,data,callback) {
     callback(result);
   });
 }
+
+router.get('/count/:service',(req,res,next)=>{
+	
+	// Use connect method to connect to the server
+	MongoClient.connect(url, function(err, client) {
+	  assert.equal(null, err);
+	  const db = client.db(dbName);
+	  db.collection(req.params.service).count((err,count)=>{
+	  	res.json({
+	  	status:'1',
+         count:count,
+         err:err,
+	  	})
+	  })
+	   
+	});
+})
+
+router.get('/delete/:service',(req,res,next)=>{
+
+	
+	// Use connect method to connect to the server
+	MongoClient.connect(url, function(err, client) {
+	  assert.equal(null, err);
+	  const db = client.db(dbName);
+	  db.collection(req.params.service).deleteMany({},(err,result)=>{
+	  	res.json({
+	  	status:'1',
+         data:result,
+         err:err,
+	  	})
+	  })
+	   
+	});
+})
 router.get('/test',(req,res,next)=>{
-	const MongoClient = require('mongodb').MongoClient;
-	const assert = require('assert');
-	 
-	// Connection URL
-	const url = 'mongodb://127.0.0.1:27017';
-	 
-	// Database Name
-	const dbName = 'fnxt';
-	 
+	
+	
 	// Use connect method to connect to the server
 	MongoClient.connect(url, function(err, client) {
 	  assert.equal(null, err);
 	  console.log("Connected successfully to server");
 	  const db = client.db(dbName);
-	 client.close()
+	  client.close()
+	  res.json({
+	  	status:'1',
+         msg:'db connected.',
+	  })
 	});
 })
 module.exports = router;
