c9b91965ccbbd9c61a4e53cec0caa249e126b6f4
peddaswamy <kpswamy540@gmail.com>
2021-02-11 18:35:03 +0530

    added size restriction

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('mongoconn',mongoconn);
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '512mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
